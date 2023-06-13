9e113d80c4977c0b618e0fd34b508b844c967967
bymfd <frukolosko@gmail.com>
2023-06-13 17:23:15 +0300

    Update Daire.java

--- patika/src/Daire/Daire.java
+++ patika/src/Daire/Daire.java
@@ -1,2 +1,19 @@
-package Daire;public class Daire {
+package Daire;
+
+import java.util.Scanner;
+
+public class Daire {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double cap, alan, cevre, pi = 3.14;
+        System.out.print("Çap: ");
+        cap = input.nextDouble();
+
+        alan = pi * cap * cap;
+        cevre = 2 * pi * cap;
+
+        System.out.println("Dairenin alanı: " + alan + "\nDairenin çevresi: " + cevre);
+
+    }
+
 }
